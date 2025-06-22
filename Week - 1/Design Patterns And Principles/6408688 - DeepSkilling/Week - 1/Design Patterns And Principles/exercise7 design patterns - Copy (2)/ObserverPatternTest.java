6c67b243003555aaf8e3ec9f1edbdbe68a29c368
DevakiVasundhara <kdevakivasundhara26@gmail.com>
2025-06-22 22:20:43 +0530

    Delete Week - 1/Design Patterns And Principles/6408688 - DeepSkilling/Week - 1/Design Patterns And Principles/exercise7 design patterns - Copy (2)/ObserverPatternTest.java

--- Week - 1/Design Patterns And Principles/6408688 - DeepSkilling/Week - 1/Design Patterns And Principles/exercise7 design patterns - Copy (2)/ObserverPatternTest.java
+++
@@ -1,8 +0,0 @@
-public class ObserverPatternTest {
-    public static void main(String[] args) {
-        StockMarket stock = new StockMarket();
-        Observer mobileApp = new MobileApp();
-        stock.registerObserver(mobileApp);
-        stock.setPrice(500.0f);
-    }
-}

765f94bb53418607d9b55311ecc421f9e8be7e55
DevakiVasundhara <kdevakivasundhara26@gmail.com>
2025-06-22 22:22:20 +0530

    Delete Week - 1/Design Patterns And Principles/6408688 - DeepSkilling/Week - 1/Design Patterns And Principles/exercise 6 design patterns - Copy - Copy/ProxyPatternTest.java

--- Week - 1/Design Patterns And Principles/6408688 - DeepSkilling/Week - 1/Design Patterns And Principles/exercise 6 design patterns - Copy - Copy/ProxyPatternTest.java
+++
@@ -1,7 +0,0 @@
-public class ProxyPatternTest {
-    public static void main(String[] args) {
-        Image image = new ProxyImage("photo.jpg");
-        image.display();
-        image.display(); // second time should not load again
-    }
-}

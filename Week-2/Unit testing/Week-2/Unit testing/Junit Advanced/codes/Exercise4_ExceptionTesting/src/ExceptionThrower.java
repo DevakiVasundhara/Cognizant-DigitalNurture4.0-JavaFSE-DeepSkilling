3a7c28bcf3e3c93c0f7d30f6cd10e5a530207714
DevakiVasundhara <kdevakivasundhara26@gmail.com>
2025-06-30 19:29:22 +0530

    Delete Week-2/Unit testing/Week-2/Unit testing/Junit Advanced/codes/Exercise4_ExceptionTesting/src/ExceptionThrower.java

--- Week-2/Unit testing/Week-2/Unit testing/Junit Advanced/codes/Exercise4_ExceptionTesting/src/ExceptionThrower.java
+++
@@ -1,5 +0,0 @@
-public class ExceptionThrower {
-    public void throwException() throws IllegalArgumentException {
-        throw new IllegalArgumentException("This is an illegal argument!");
-    }
-}

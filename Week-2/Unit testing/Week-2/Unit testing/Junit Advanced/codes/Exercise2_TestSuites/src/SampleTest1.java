73029ebabe980e0a4341ce205a364504a2ad4d11
DevakiVasundhara <kdevakivasundhara26@gmail.com>
2025-06-30 19:30:25 +0530

    Delete Week-2/Unit testing/Week-2/Unit testing/Junit Advanced/codes directory

--- Week-2/Unit testing/Week-2/Unit testing/Junit Advanced/codes/Exercise2_TestSuites/src/SampleTest1.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
-
-public class SampleTest1 {
-    @Test
-    void sampleTestOne() {
-        assertEquals(1, 1);
-    }
-}
--- Week-2/Unit testing/Week-2/Unit testing/Junit Advanced/codes/Exercise2_TestSuites/src/SampleTest2.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
-
-public class SampleTest2 {
-    @Test
-    void sampleTestTwo() {
-        assertEquals(2, 2);
-    }
-}
